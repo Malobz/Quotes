e5f80cdc6a35c2b0cc19a098b1ab10e16247ae45
Mercy Maloba <mercy.maloba@student.moringaschool.com>
2021-07-20 04:13:11 +0300

    delete quote array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,5 @@ import { Quote } from './quote';
 })
 export class AppComponent {
 
-  // quotes:Quote[] = [
-  //   {Name:'Maloba Mercy', quote:'The purpose of our lives is to be happy.',author:'Dalai Lama'},
-  //   {Name:'Eugene Martins', quote:'Life is what happens when you’re busy making other plans.',author:'John Lenon'},
-  //   {Name:'Maureen Hope', quote:'Get busy living or get busy dying.',author:'Stephen King'},
-  // ];
-  // quotes:string[];
-  // constructor(){
-  //   this.quotes = ['The purpose of our lives is to be happy.','Life is what happens when you’re busy making other plans.','Get busy living or get busy dying.']
-  // }
+  
 }
