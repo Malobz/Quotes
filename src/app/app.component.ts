3e02e3ec60de16d533c6603c900840dfd6b8061d
Mercy Maloba <mercy.maloba@student.moringaschool.com>
2021-07-20 04:49:04 +0300

    add component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,9 @@ import { Quote } from './quote';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
 
   
 }
