5e407c4a70abe174f14b7c0d8947e131f459cf85
Mercy Maloba <mercy.maloba@student.moringaschool.com>
2021-07-20 04:10:17 +0300

    delete classes

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -3,7 +3,5 @@ export class Quote {
     constructor(public Name:string, public quote: string, public author:string, public completeDate: Date, public upvote:number, public downvote:number){
         this.showDescription=false;
     }
-    // Name: string
-    // quote: string
-    // author: string
+   
 }
